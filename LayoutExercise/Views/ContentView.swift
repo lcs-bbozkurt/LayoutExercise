7b2a61ea34a0c6b0ffa6bc60a16c15575490aab3
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-10-28 19:24:09 -0400

    BETTER

--- LayoutExercise/Views/ContentView.swift
+++ LayoutExercise/Views/ContentView.swift
@@ -9,25 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            
-        }
         VStack {
+            Text("TITLE")
+                .fontWeight(.black)
+              
+        
+        HStack {
             
             Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
                 .fontWeight(.black)
            
-        }
-        VStack {
-            
-            Text("Et malesuada fames ac turpis. Justo nec ultrices dui sapien eget mi proin sed libero. Cursus metus aliquam eleifend mi in. Feugiat nibh sed pulvinar proin gravida hendrerit. Aliquam id diam maecenas ultricies mi eget mauris. Ornare massa eget egestas purus viverra accumsan in nisl. Amet luctus venenatis lectus magna. At quis risus sed vulputate odio. Lacus suspendisse faucibus interdum posuere lorem. Turpis egestas integer eget aliquet nibh praesent tristique magna sit. Consequat mauris nunc congue nisi vitae suscipit. At auctor urna nunc id cursus metus aliquam eleifend mi. Sit amet venenatis urna cursus.")
-                .fontWeight(.black)
+            }
         }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
