2a17a5f53e3a65c497d6395c3f6f1aadc8207605
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-10-28 20:23:10 -0400

    example 1 done

---
+++ LayoutExercise/Views/ContentView2.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView2.swift
+//  LayoutExercise
+//
+//  Created by Berk Bozkurt on 2021-10-28.
+//
+
+import SwiftUI
+
+struct ContentView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView2_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView2()
+    }
+}
