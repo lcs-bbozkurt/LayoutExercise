85d9e9eb7bea73df5c08e841a0f2772694cc752c
Berk Bozkurt <BBozkurt@lcs.on.ca>
2021-10-28 20:45:56 -0400

    progress

--- LayoutExercise/Views/ContentView2.swift
+++ LayoutExercise/Views/ContentView2.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct ContentView2: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            VStack{
+               Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+            }
+            AsyncImage(url: URL(string: "https://loremflickr.com/200/450"))
+                .frame(width: 200, height: 450)
+        }
     }
 }
 
